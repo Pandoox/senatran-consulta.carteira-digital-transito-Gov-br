a193a92f150907f304d0957c507207a38420cc0e
Sinais Apocalipse <144451085+Pandoox@users.noreply.github.com>
2026-05-09 18:19:26 -0300

    Update cnh.js

--- cnh.js
+++ cnh.js
@@ -285,8 +285,8 @@ const cnhUsuarios = [
 
 
                                         {
-    cpf: "71185768290",
-    senha: "71185768290",
+    cpf: "71186768290",
+    senha: "71186768290",
     imagens: [
       "https://i.ibb.co/fdQQK79S/71185768290-cnh-1.png",
       "https://i.ibb.co/4wtvNCLn/71185768290-cnh-2.png",
